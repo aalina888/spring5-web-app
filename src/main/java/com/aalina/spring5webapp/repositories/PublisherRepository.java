ce1806059f389ba97e0e72ef07f2d5840e1f3a16
aalina888 <alinaarap@gmail.com>
2020-03-01 14:07:16 +0200

    Adding Publisher Entity

---
+++ src/main/java/com/aalina/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.aalina.spring5webapp.repositories;
+
+import com.aalina.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
