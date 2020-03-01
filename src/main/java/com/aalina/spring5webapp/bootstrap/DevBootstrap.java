8539c43da44bc61fa05ddc1f6b6b7c573417d623
aalina888 <alinaarap@gmail.com>
2020-03-01 13:56:18 +0200

    Bootstrap 2

--- src/main/java/com/aalina/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/aalina/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,10 +2,22 @@ package com.aalina.spring5webapp.bootstrap;
 
 import com.aalina.spring5webapp.model.Author;
 import com.aalina.spring5webapp.model.Book;
+import com.aalina.spring5webapp.repositories.AuthorRepository;
+import com.aalina.spring5webapp.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
     private void initData() {
         // Eric
         Author eric = new Author("Eric", "Evans");
@@ -13,10 +25,16 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
         // Rod
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "23444", "Work");
         rod.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
     }
 
     @Override
