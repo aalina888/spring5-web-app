3b0bfd04f14ae6b7b1312be81714dce02824615f
aalina888 <alinaarap@gmail.com>
2020-03-07 21:32:21 +0200

    Overriding equals(), hashCode() and toString while working with sets

--- src/main/java/com/aalina/spring5webapp/controllers/AuthorController.java
+++ src/main/java/com/aalina/spring5webapp/controllers/AuthorController.java
@@ -1,7 +1,6 @@
 package com.aalina.spring5webapp.controllers;
 
 import com.aalina.spring5webapp.repositories.AuthorRepository;
-import com.aalina.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
